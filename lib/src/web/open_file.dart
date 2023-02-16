fbb4f785150e49785ed3f985a96d0df4b5b5f720
shadat rahman <shadat.rahman.464@gmail.com>
2023-02-16 21:15:05 +0600

    changed to open_file_plus

--- lib/src/web/open_file.dart
+++ lib/src/web/open_file.dart
@@ -1,18 +1,12 @@
 import 'dart:async';
-import 'package:open_file/src/common/open_result.dart';
+import 'package:open_file_plus/src/common/open_result.dart';
 
 import 'web.dart' as web;
 
 class OpenFile {
   OpenFile._();
-  static Future<OpenResult> open(String? filePath,
-      {String? type,
-        String? uti,
-        String linuxDesktopName = "xdg",
-        bool linuxByProcess = false}) async {
+  static Future<OpenResult> open(String? filePath, {String? type, String? uti, String linuxDesktopName = "xdg", bool linuxByProcess = false}) async {
     final _b = await web.open("file://$filePath");
-    return OpenResult(
-        type: _b ? ResultType.done : ResultType.error,
-        message: _b ? "done" : "there are some errors when open $filePath");
+    return OpenResult(type: _b ? ResultType.done : ResultType.error, message: _b ? "done" : "there are some errors when open $filePath");
   }
 }
